ff48f2da5ab534abce7c0292b6ba33d9e59ff7b1
remove <Sandip@Sandip-PC>
2019-01-25 13:17:55 +0530

    second level cache Added

--- DemoHib/src/com/jotiram/secondlevelcache/Alien.java
+++ DemoHib/src/com/jotiram/secondlevelcache/Alien.java
@@ -1,13 +1,19 @@
 package com.jotiram.secondlevelcache;
 
+import javax.persistence.Cacheable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
 @Entity
 @Table(name="alien_table")
+@Cacheable
+@Cache(usage=CacheConcurrencyStrategy.READ_ONLY)
 public class Alien {
 	
 	@Id
--- DemoHib/src/com/jotiram/secondlevelcache/App.java
+++ DemoHib/src/com/jotiram/secondlevelcache/App.java
@@ -1,5 +1,37 @@
 package com.jotiram.secondlevelcache;
 
-public class App {
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
 
+public class App {
+	
+	public static void main(String[] args) {
+	
+		Alien a=null;
+		
+		Configuration con=new Configuration().configure().addAnnotatedClass(Alien.class);
+		ServiceRegistry reg=new ServiceRegistryBuilder().applySettings(con.getProperties()).buildServiceRegistry();
+		
+		SessionFactory sf=con.buildSessionFactory(reg);
+		Session session1=sf.openSession();
+		session1.beginTransaction();
+		
+		a=(Alien) session1.get(Alien.class, 1);
+		System.out.println(a);
+		
+		session1.getTransaction().commit();
+		session1.close();
+		
+		Session session2=sf.openSession();
+		session2.beginTransaction();
+		
+		a=(Alien) session2.get(Alien.class, 1);
+		System.out.println(a);
+				
+		session2.getTransaction().commit();
+		session2.close();
+}
 }
